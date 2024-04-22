1d3fddb23df04b9e7746758a83db01482192f2a8
iwarrie <160537393+iwarrie@users.noreply.github.com>
2024-04-22 11:12:53 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+# Pseudo code for hello_world.py
+
+# Prompt the user to enter their name using input()
+# Save the user's name in a variable
+
+# Prompt the user to enter their age using input()
+# Save the user's age in a variable
+
+# Print the user's name
+# Print the user's age
+# Print "Hello World!" on a new line
+
+# Actual code for hello_world.py
+
+# Prompt the user to enter their name
+user_name = input("Enter your name: ")
+
+# Prompt the user to enter their age
+user_age = input("Enter your age: ")
+
+# Print the user's name
+print("Your name is:", user_name)
+
+# Print the user's age
+print("Your age is:", user_age)
+
+# Print "Hello World!" on a new line
+print("Hello World!")
